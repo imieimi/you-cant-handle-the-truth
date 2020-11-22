d040a0364f643fa700f3919b0c3ac8985e835da4
Eimi <eeimikoga@gmail.com>
2020-11-21 22:57:06 -0800

    commit 2

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,15 +1,41 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class HandleTruthTest {
-    /*@Test
+    @Test
     public void makeshiftTest() {
         HandleTruth.wordCount();
     }
-   */
 
+    //test 1
     @Test
-    public void myTests(){
-        HandleTruth.wordCount();
+    public void checkForYou(){
+
+    }
+
+    //test 2
+    @Test
+    public void checkForTruth(){
+
+    }
+
+    //test 3
+    @Test
+    public void checkTwo(){
+
+    }
+
+    //test 4
+    @Test
+    public void checkFive(){
+
+    }
+
+    //test 5
+    @Test
+    public void totalNum(){
 
     }
 }
